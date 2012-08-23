d9b45b3255d1b1e12a52ac340fb06719fa43f647
mikegee <gee.24@osu.edu>
2012-08-23 07:40:30 -0400

    rspec works

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
-
-# Specify your gem's dependencies in ruby-egg-notifier.gemspec
+source :rubygems
 gemspec
+gem 'rake'
+gem 'rspec'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'egg-notifier'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.order = 'random'
+end
